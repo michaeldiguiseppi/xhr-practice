7f2d0aeee0ac436a43163246795ba73ee0c816dc
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-01-18 16:46:16 -0700

    Added solution for XHR practice

--- client.js
+++ client.js
@@ -1,1 +1,44 @@
-var usersRequest = new XMLHttpRequest();
+// var usersRequest = new XMLHttpRequest();
+// var div = document.getElementsByTagName('div');
+
+// usersRequest.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//         var userListings = JSON.parse(this.responseText);
+//         console.log(userListings);
+//         for (var i = 0; i < userListings.data.children.length; i++) {
+//             var userListing = userListings.data.children[i];
+//             console.log(userListing.data.title);
+//         };
+//     };
+// };
+
+// usersRequest.open('GET', 'http://localhost:3000/users');
+
+// usersRequest.send();
+
+var userListings = null;
+
+var data2 = document.getElementById('total-likes');
+
+var xhr = new XMLHttpRequest();
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    userListings = JSON.parse(this.responseText);
+    data2.innerText = '';
+    for (var i = 0; i < userListings.length; i++) {
+        var ul = document.createElement('ul');
+        var li = document.createElement('li');
+        li.innerText = (userListings[i].name + ': ' + userListings[i].likes);
+        data2.appendChild(ul);
+        ul.appendChild(li);
+    };
+
+  }
+});
+
+xhr.open('GET', 'http://localhost:3000/users');
+xhr.send();
+
+
+
